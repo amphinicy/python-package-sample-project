25f85d9a753c2a2b8fde237cd81654e439f634d8
Ivan Arar <ivanarar@Ivans-MacBook-Pro.local>
2019-03-08 17:06:47 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='sample',
+    version='1.0.0',
+
+    description='A sample project on how to make a python package',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/amphinicy/python-package-sample-project',
+    licence='MIT',
+
+    author='Ivan Arar',
+    author_email='ivan.arar@amphinicy.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='sample, python, package',
+
+    packages=find_packages(),
+    install_requires=[
+        'click~=6.7',
+    ],
+
+    project_urls={
+        'Source': 'https://github.com/amphinicy/python-package-sample-project',
+    },
+
+    entry_points={
+        'console_scripts': [
+            'sample=sample.cli:cli'
+        ],
+    },
+)
