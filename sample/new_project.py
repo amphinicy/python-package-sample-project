adc37286ef4e2d985c61b874b410801a4829eaf9
Ivan Arar <ivan.arar@amphinicy.com>
2019-04-08 12:42:58 +0200

    feature/gitlab-ci-support

--- sample/new_project.py
+++ sample/new_project.py
@@ -26,6 +26,9 @@ TRAVIS = '.travis.yml'
 TRAVIS_TEMPLATE = 'travis.yml.template'
 TRAVIS_PYPI_TEMPLATE = 'travis_pypi.yml.template'
 
+GITLAB_CI = '.gitlab-ci.yml'
+GITLAB_CI_TEMPLATE = 'gitlab-ci.yml.template'
+
 PROJECT_INIT = '__init__.py'
 
 
@@ -89,6 +92,8 @@ class NewProject:
         self._use_pypi = False
         self._pypi_username = None
 
+        self._use_gitlab_ci = False
+
         self._project_root_path = None
         self._sample_lib_root_path = None
         self._project_app_dir = None
@@ -112,6 +117,8 @@ class NewProject:
         if self._use_travis:
             self._use_pypi = click.confirm('Do you want to upload your project to pypi.org?')
             self._pypi_username = click.prompt('Enter your pypi.org username')
+
+        self._use_gitlab_ci = click.confirm('Do you want to use Gitlab CI?')
 
     @register_step(index=1, divider_down=lambda: click.echo())
     def _clone_git_repo(self) -> None:
@@ -185,21 +192,32 @@ class NewProject:
 
             f.write(f'{travis_template}\n{travis_pypi_template}')
 
-    @register_step(index=5, rollback='_remove_git_repo')
+    @register_step(index=5, validator='_use_gitlab_ci', rollback='_remove_git_repo')
+    def _create_gitlab_ci_yml(self) -> None:
+        """Creating .gitlab-ci.yml ..."""
+
+        with open(os.path.join(self._sample_lib_root_path, GITLAB_CI_TEMPLATE)) as f:
+            t = Template(f.read())
+            gitlab_ci_template = t.safe_substitute(project_name=self._project_name)
+
+        with open(os.path.join(self._project_root_path, GITLAB_CI), 'w') as f:
+            f.write(gitlab_ci_template)
+
+    @register_step(index=6, rollback='_remove_git_repo')
     def _create_app_dir(self) -> None:
         """Creating python package app dir ..."""
 
         self._project_app_dir = os.path.join(self._project_root_path, self._project_name)
         os.makedirs(self._project_app_dir)
 
-    @register_step(index=6, rollback='_remove_git_repo')
+    @register_step(index=7, rollback='_remove_git_repo')
     def _create_init(self) -> None:
         """Creating __init__.py ..."""
 
         with open(os.path.join(self._project_app_dir, PROJECT_INIT), 'w') as f:
             f.write('')
 
-    @register_step(index=7, rollback='_remove_git_repo')
+    @register_step(index=8, rollback='_remove_git_repo')
     def _create_cli(self) -> None:
         """Creating cli.py ..."""
 
@@ -210,7 +228,7 @@ class NewProject:
         with open(os.path.join(self._project_app_dir, CLI), 'w') as f:
             f.write(setup_template)
 
-    @register_step(index=8, rollback='_remove_git_repo')
+    @register_step(index=9, rollback='_remove_git_repo')
     def _create_tests(self) -> None:
         """Creating tests ..."""
 
@@ -227,7 +245,7 @@ class NewProject:
         with open(os.path.join(tests_dir, TEST.format(self._project_name)), 'w') as f:
             f.write(test_template)
 
-    @register_step(index=9, rollback='_remove_git_repo',
+    @register_step(index=10, rollback='_remove_git_repo',
                    divider_down=lambda: click.echo())
     def _create_virtual_environment(self) -> None:
         """Creating virtual environment ..."""
@@ -273,11 +291,13 @@ class NewProject:
                 step.fn(self)
             except Exception as e:
                 click.echo()
-                click.secho(f'An error occurred: {str(e)}.')
+                click.secho(f'An error occurred: {str(e)}.', fg='red')
 
                 if step.rollback:
-                    click.secho('Reverting!')
+                    click.secho('Reverting!', fg='red')
                     getattr(self, step.rollback)()
+
+                click.echo()
 
                 sys.exit()
 
